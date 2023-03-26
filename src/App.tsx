91b0974ff081a3a9a13f2fcd6ab164eafd65fdde
haeun kim <ckhe1215@gmail.com>
2023-03-27 00:32:23 +0900

    make static page

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,48 @@
+import restaurantData from "../restaurants.json"
+import "./styles.css"
+
 export default function App() {
   return (
-    <p>
-      과제를 진행해 주세요.
-    </p>
+    <>
+      <h1>오늘의 메뉴</h1>
+      <div>
+        <label htmlFor="search-bar" className="paddingRight">검색</label>
+        <input type="text" id="search-bar" placeholder="식당 이름"/>
+        <ul className="buttonList">
+          <li><button type="button">전체</button></li>
+          <li><button type="button">중식</button></li>
+          <li><button type="button">한식</button></li>
+          <li><button type="button">일식</button></li>
+        </ul>
+      </div>
+      <table>
+        <thead>
+          <th className="paddingInline">식당 이름</th>
+          <th>종류</th>
+          <th>메뉴</th>
+        </thead>
+        <tbody>
+          {restaurantData.restaurants.map((item) =>
+            <tr>
+              <td>
+                {item.name}
+              </td>
+              <td>
+                {item.category}
+              </td>
+              <td>
+                <ul>
+                  {
+                    item.menu.map((menu) =>
+                      <li>{menu.name + "(" + menu.price.toLocaleString('ko-KR') +  "원)"}</li>
+                    )
+                  }
+                </ul>
+              </td>
+            </tr>
+          )}
+        </tbody>
+      </table>
+    </>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+
 function main() {
-  // TODO: App 컴포넌트를 render 해주세요.
+  const root = document.getElementById('root');
+  if (!root) {
+    return;
+  }
+
+  ReactDOM.createRoot(root).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+  );
 }
 
 main();
